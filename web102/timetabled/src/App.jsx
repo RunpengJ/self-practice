20134abf9e46c9fe3b8cd5a5b86a74348a92a781
RunpengJ <runpengj@gmail.com>
2025-06-04 18:21:10 -0700

    initial commit

---
+++ web102/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in San Diego</h1>
+      <h2>Welcome to San Diego!</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
